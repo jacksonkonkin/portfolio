5f982b121c32f057df8edd6dc63dd66aaac7a2eb
jackson <jacksonkonkin@gmail.com>
2023-07-13 14:36:31 -0700

    added header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/headshot.png'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Jackson Konkin</h1>
+        <h5 className="text-light">Software Engineering Student</h5>
+        <CTA />
+
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
+
+    </header>
   )
 }
 
